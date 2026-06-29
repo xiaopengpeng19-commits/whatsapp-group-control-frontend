cb484adadb50917e1fccce7f16fc0658411a4855
xiaopengpeng19-commits <xiaopengpeng19@gmail.com>
2026-06-29 23:10:48 +0800

    路由

--- src/api/whatsapp.js
+++ src/api/whatsapp.js
@@ -1,18 +1,16 @@
-
-import api from './index'
-
+// frontend/src/api/whatsapp.js
 export default {
   // 账号管理
-  getAccounts: () => api.get('/whatsapp/accounts'),
-  addAccount: (data) => api.post('/whatsapp/accounts', data),
-  deleteAccount: (account) => api.delete(`/whatsapp/accounts/${account}`),
+  getAccounts: () => api.get('/whatsapp/accounts/list'),      // 改
+  addAccount: (data) => api.post('/whatsapp/accounts/add'),  // 改
+  deleteAccount: (account) => api.post(`/whatsapp/accounts/${account}/del`),  // 改
   online: (account) => api.post(`/whatsapp/accounts/${account}/online`),
   offline: (account) => api.post(`/whatsapp/accounts/${account}/offline`),
   getQRCode: (params) => api.get('/whatsapp/qrcode', { params }),
   
   // 消息
-  sendMessage: (data) => api.post('/whatsapp/messages', data),
-  getMessages: (params) => api.get('/whatsapp/messages', { params }),
+  sendMessage: (data) => api.post('/whatsapp/messages/send'),  // 改
+  getMessages: (params) => api.get('/whatsapp/messages/get'),  // 改
   
   // 联系人
   getContacts: (params) => api.get('/whatsapp/contacts', { params })
